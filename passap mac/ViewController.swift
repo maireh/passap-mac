15630c342c1a10886e1b059d41a054cab86e6ac5
maireh <maire_h@yahoo.com>
2018-05-31 06:50:13 -0700

    Initial Commit

---
+++ passap mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  passap mac
+//
+//  Created by Maire Howard on 5/31/18.
+//  Copyright © 2018 k2tog. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
